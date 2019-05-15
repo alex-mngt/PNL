5395776838c3c855d07ed53f67ed8d23e216f5ec
alex-mngt <mongeot.alex@gmail.com>
2019-05-15 17:20:56 +0200

    embed player

--- index.php
+++ index.php
@@ -10,12 +10,11 @@
 </head>
 
 <body>
+    <div id="ytplayer"></div>
     <div id="fond_site">
         <div id="champ">
             <form method="POST">
-                <label for="mdp">Mot de passe requis :</label>
-                <br>
-                <input type="password" name="mdp">
+                <input type="password" name="mdp" placeholder="Mot de passe requis :">
             </form>
             <?php
 
@@ -44,6 +43,36 @@ try{
     ?>
         </div>
     </div>
+
+    <script>
+        // Load the IFrame Player API code asynchronously.
+        var tag = document.createElement('script');
+        tag.src = "https://www.youtube.com/player_api";
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+        // Replace the 'ytplayer' element with an <iframe> and
+        // YouTube player after the API code downloads.
+        var player;
+        function onYouTubePlayerAPIReady() {
+            player = new YT.Player('ytplayer', {
+            height: '360',
+            width: '640',
+            videoId: 'z3avlLlGZkA',
+            playerVars : {
+                autoplay: 1,
+                controls: 0,
+                loop: 1,
+                playlist: 'z3avlLlGZkA',
+            },
+            events: {
+                'onReady': () => {
+                    document.getElementById('ytplayer').style.opacity = 1;
+                }
+            }
+            });
+        }
+</script>
 </body>
 
 </html>
